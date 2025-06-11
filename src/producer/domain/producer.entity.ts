0cd2c6e56147ea6b23f53194f2d2df7313b86a8b
Melqui Franca <melquifranca@hotmail.com>
2025-06-11 21:29:49 +0000

    reorganiza estrutura de pastas

---
+++ src/producer/domain/producer.entity.ts
@@ -0,0 +1,52 @@
+export type ProducerProps = {
+  producerId?: Number
+  name: string
+  identifier: string
+}
+
+export type ProducerCreateCommand = {
+  name: string
+  identifier: string
+}
+
+class Identifier {
+  constructor (public readonly value: string) {
+    if (!Identifier.validate(value)) {
+      throw new Error('Invalid identifier format. It must be an 11-digit number.')
+    }
+  }
+  static validate(identifier: string): boolean {
+    return /^\d{11}$/.test(identifier) || /^\d{14}$/.test(identifier)
+  }
+  getValue(): string {
+    return this.value
+  }
+}
+
+export class Producer {
+  producerId?: Number
+  name: string
+  identifier: Identifier
+  constructor (props: ProducerProps) {
+    this.producerId = props.producerId
+    this.name = props.name
+    this.identifier = new Identifier(props.identifier)
+  }
+  // factory method
+  static create (props: ProducerCreateCommand): Producer {
+    return new Producer(props)
+  }
+  changeName (name: string): void {
+    this.name = name
+  }
+  changeIdenfifier (identifier: string): void {
+    this.identifier = new Identifier(identifier)
+  }
+  toJSON () {
+    return {
+      producerId: this.producerId,
+      name: this.name,
+      identifier: this.identifier.getValue()
+    }
+  }
+}
