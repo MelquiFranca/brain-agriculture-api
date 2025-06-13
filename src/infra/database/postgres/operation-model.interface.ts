c0a0b4dae5a2bd28fd0114507c277a85b081ab46
Melqui Franca <melquifranca@hotmail.com>
2025-06-13 19:17:59 +0000

    cria interface para model e operacoes postgres sql

---
+++ src/infra/database/postgres/operation-model.interface.ts
@@ -0,0 +1,11 @@
+import { IModel } from "./model.interface"
+
+export type QueryModelResult = { text: string, values: any[] }
+export interface IOperationsModel {
+  getInsertQuery (model: IModel): QueryModelResult
+  createTableQuery (): string
+  getUpdateQuery (model: IModel): QueryModelResult
+  getDeleteQuery (modelId: string): QueryModelResult
+  getSelectByIdQuery (modelId: Number): QueryModelResult
+  getSelectAllQuery (): string
+}
