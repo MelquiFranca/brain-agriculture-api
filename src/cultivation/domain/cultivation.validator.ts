d37632d2626df8573570f318938b16efd8a4df65
Melqui Franca <melquifranca@hotmail.com>
2025-06-13 16:37:04 +0000

    cria validador da entidade cultivation

---
+++ src/cultivation/domain/cultivation.validator.ts
@@ -0,0 +1,15 @@
+import IValidatorFields from '@base/shared/validators/validator-fields-interface'
+import { Cultivation } from './cultivation.entity'
+
+export class CultivationValidator implements IValidatorFields {
+  validate(entity: Cultivation): string[] {
+    const errors = []
+    if (!entity.name || entity.name.length > 100) {
+      errors.push('Name must be a non-empty string with a maximum length of 100 characters.')
+    }
+    if (entity.cultivationId && !Number.isInteger(entity.cultivationId)) {
+      errors.push('Cultivation ID must be a number.')
+    }
+    return errors
+  }
+}
