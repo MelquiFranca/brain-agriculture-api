9655e7966d6eab4c946935bd11090310f62f4216
Melqui Franca <melquifranca@hotmail.com>
2025-06-13 21:09:22 +0000

    implementa controller para producer

---
+++ src/producer/application/producer.controller.ts
@@ -0,0 +1,43 @@
+import { IController } from "@base/shared/application/controller.interface"
+import { Request, Response } from "express"
+import { CreateProducerUseCase } from "./create-producer.use-case"
+import { IRepository } from "@base/shared/repository.interface"
+import { Producer } from "../domain/producer.entity"
+import { ListProducerUseCase } from "./list-producer.use-case"
+import { FindProducerUseCase } from "./find-producer.use-case"
+import { DeleteProducerUseCase } from "./delete-producer.use-case"
+import { UpdateProducerUseCase } from "./update-producer.use-case"
+
+export class ProducerController implements IController {
+  constructor (private readonly repository: IRepository<Producer>) {
+  }
+  async create(request: Request, response: Response): Promise<any> {
+    const { body } = request
+    const createProducerUseCase = new CreateProducerUseCase(this.repository)
+    const result = await createProducerUseCase.execute(body)
+    response.status(201).send(result)
+  }
+  async list(request: Request, response: Response): Promise<any> {
+    const listProducerUseCase = new ListProducerUseCase(this.repository)
+    const result = await listProducerUseCase.execute({})
+    response.status(200).send(result)
+  }
+  async findOne(request: Request, response: Response): Promise<any> {
+    const { id } = request.params
+    const findProducerUseCase = new FindProducerUseCase(this.repository)
+    const result = await findProducerUseCase.execute({ id: Number(id) })
+    response.status(200).send(result)
+  }
+  async delete(request: Request, response: Response): Promise<any> {
+    const { body } = request
+    const deleteProducerUseCase = new DeleteProducerUseCase(this.repository)
+    const result = await deleteProducerUseCase.execute(body)
+    response.status(200).send(result)
+  }
+  async update(request: Request, response: Response): Promise<any> {
+    const { body } = request
+    const updateProducerUseCase = new UpdateProducerUseCase(this.repository)
+    const result = await updateProducerUseCase.execute(body)
+    response.status(201).send(result)
+  }
+}
